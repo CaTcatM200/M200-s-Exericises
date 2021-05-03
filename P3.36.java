a9b602e9b15e57cbdb6cbc098a2514d44c65f3e4
NekoM200 <2207752527@qq.com>
2021-05-03 14:10:44 +0800

    Add files via upload
    P-3.36 Write a Java program for a matrix class that can add and multiply arbitrary twodimensional arrays of integers.

---
+++ P3.36.java
@@ -0,0 +1,52 @@
+public class P336 {
+    public class matrix
+    {
+        public int[][] matrixadd(int[][]alphamatrix,int[][]betamatrix) throws IllegalArgumentException,NullPointerException
+        {
+            if(alphamatrix == null||betamatrix == null)
+            {
+                throw new NullPointerException();
+            }
+            if(alphamatrix.length != betamatrix.length)
+            {
+                throw new IllegalArgumentException();
+            }
+            int i,j;
+            int[][]resultmatrix = new int[alphamatrix.length][betamatrix.length];
+            for(i=0;i < resultmatrix.length;i++)
+            {
+                for(j=0;j < resultmatrix.length;j++)
+                {
+                    resultmatrix[i][j] = alphamatrix[i][j] + betamatrix[i][j];
+                }
+            }
+            return resultmatrix;
+        }
+
+        public int[][] matrixmul(int[][]alphamatrix,int[][]betamatrix) throws IllegalArgumentException,NullPointerException
+        {
+            if(alphamatrix == null||betamatrix == null)
+            {
+                throw new NullPointerException();
+            }
+            if(alphamatrix.length != betamatrix[0].length)
+            {
+                throw new IllegalArgumentException();
+            }
+            int i,j,k;
+            int[][]resultmatrix = new int[alphamatrix.length][betamatrix[0].length];
+            for(i=0;i < alphamatrix.length;i++)
+            {
+                for(j=0;j < betamatrix[0].length;j++)
+                {
+                    resultmatrix[i][j] = 0;
+                    for(k=0;k < alphamatrix[0].length;k++)
+                    {
+                        resultmatrix[i][j] = resultmatrix[i][j] + alphamatrix[i][j] * betamatrix[i][j];
+                    }
+                }
+            }
+            return resultmatrix;
+        }
+    }
+}
